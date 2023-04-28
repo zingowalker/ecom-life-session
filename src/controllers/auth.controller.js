0ca21c33b72bd77e8d375d79be1810897dc696ff
zingowalker <89342826+zingowalker@users.noreply.github.com>
2023-04-28 12:38:21 +0000

    user loggedIn and Out

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -8,6 +8,12 @@ export const cookieOptions = {
   httpOnly: true,
 }
 
+/******************************************************
+ * @SIGNUP
+ * @route http://localhost:5000/api/auth/signup
+ * @description User signUp Controller for creating new user
+ * @returns User Object
+ ******************************************************/
 
 export const signUp = asyncHandler(async (req, res) => {
   // get data from user
@@ -51,4 +57,46 @@ export const signUp = asyncHandler(async (req, res) => {
   })
 
 
+})
+
+export const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+
+  // validation
+  if (!email || !password) {
+    throw new CustomError("Please fill all details", 400)
+  }
+
+  const user = User.findOne({ email }).select("+password")
+
+  if (!user) {
+    throw new CustomError("Invalid credentials", 400)
+  }
+
+  const isPasswordMatched = await User.comparePassword(password)
+
+  if (isPasswordMatched) {
+    const token = user.getJWTtoken()
+    user.password = undefined
+    res.cookie("token", token, cookieOptions)
+    return res.status(200).json({
+      success: true,
+      token,
+      user
+    })
+  }
+
+  throw new CustomError("Password is incorrect", 400)
+})
+
+export const logout = asyncHandler(async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true
+  })
+
+  res.status(200).json({
+    success: true,
+    message: "Logged Out"
+  })
 })
