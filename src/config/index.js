871e9b1580d745dc402ba97133c872fae1cb0ec5
zingowalker <89342826+zingowalker@users.noreply.github.com>
2023-05-02 16:00:13 +0000

    image upload to s3

--- src/config/index.js
+++ src/config/index.js
@@ -3,10 +3,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  PORT: process.env.port || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost:24017/ecom",
-  JWT_SECRET: process.env.JWT_SECRET || "password@#202303",
-  JWT_EXPIRY: process.env.JWT_EXPIRY || "15d",
+  PORT: process.env.port,
+  MONGODB_URL: process.env.MONGODB_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRY: process.env.JWT_EXPIRY,
+  S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+  S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+  S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+  S3_REGION: process.env.S3_REGION,
 }
 
 export default config;
